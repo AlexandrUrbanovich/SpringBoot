e9d924d38d2c04f04ef01c0acf09cf7237f050d6
aliaksandr_urbanovich <aliaksandr_urbanovich@epam.com>
2020-01-21 17:51:28 +0300

    add permission for all users

--- src/main/java/com/dictionary/example/repository/EnWordRepository.java
+++ src/main/java/com/dictionary/example/repository/EnWordRepository.java
@@ -4,7 +4,11 @@ import com.dictionary.example.models.EnWord;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnWordRepository extends CrudRepository<EnWord, Long> {
+
+    List<EnWord> findByWord(String word);
 
 }
