cead7de5094c898ee42c8f2a83745f65e6d543fd
aliaksandr_urbanovich <aliaksandr_urbanovich@epam.com>
2020-02-14 16:21:38 +0300

    add validation for Word

--- src/main/java/com/dictionary/example/controllers/RegistrationController.java
+++ src/main/java/com/dictionary/example/controllers/RegistrationController.java
@@ -5,8 +5,12 @@ import com.dictionary.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -20,10 +24,18 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model) {
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+
+            model.mergeAttributes(errors);
+
+            return "registration";
+        }
 
         if(!userService.addUser(user)) {
-            model.addAttribute("message", "User exists!");
+            model.addAttribute("usernameError", "User exists!");
             return "registration" ;
         }
 
--- src/main/resources/templates/login.ftl
+++ src/main/resources/templates/login.ftl
@@ -4,6 +4,11 @@
 <@c.page>
 <div class="mb-1">Log in</div>
 
+<#if Session?? && Session.SPRING_SECURITY_LAST_EXCEPTION??>
+    <div class="alert alert-danger" role="alert">
+        ${Session.SPRING_SECURITY_LAST_EXCEPTION.message}
+    </div>
+
 <@l.login "/login" true />
 
 </@c.page>
